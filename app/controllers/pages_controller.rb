737c6751f0ee8a4d6b8cd85e2153c798696738a0
iantheninja <munene.ian@live.com>
2020-11-13 21:02:06 +0300

    update page action updates successfuly

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,16 +6,30 @@ class PagesController < ApplicationController
   def show
     @page = Page.find(params[:id])
   end
-  
+
   def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to page_path(@page)
+    else
+      render 'edit'
+    end
   end
 
   def edit
+    @page = Page.find(params[:id])
   end
 
   def destroy
   end
 
   def delete
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:id, :name, :permalink, :position, :visible, :content)
   end
 end
